da843afe11a2e944b7b5c79811dd2026e091cbd8
PaulDirac33 <riccardo.bizzi.9mm@gmail.com>
2023-09-04 15:43:02 +0200

    arduino_GUI.py 1.3
    The interface can be adapted to other screen resolution, changing the value of "a".

--- python/GUI/arduino_GUI.py
+++ python/GUI/arduino_GUI.py
@@ -14,58 +14,60 @@ import os
 import subprocess
 import time
 
-Window.size = (460,660)
+a = 1 # Scale, let 1 for 1920x1080 thus set 2 for 2560x1600
+
+Window.size = (455,660)
 class Arduino(App):
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
 
-        self.title_label = Label(text="Arduino acquisition", height=50, bold=True, font_size=50, color=(0, 0.5, 1, 0.8))
+        self.title_label = Label(text="Arduino acquisition", height=a*50, bold=True, font_size=a*50, color=(0, 0.5, 1, 0.8))
         self.layout.add_widget(self.title_label)
         self.data = self.load_json_data("/Users/riccardo/github/Coding/config/arduino.json")
         self.text_inputs = {}
-        Window.clearcolor = (0.1, 0.1, 0.1, 1)  # Imposta il colore di sfondo
+        Window.clearcolor = (0.1, 0.1, 0.1, 1)
         
         for section_name, section_data in self.data.items():
-            section_label = Label(text=section_name, bold=True, font_size=40, color=(1, 1, 1, 0.8))
+            section_label = Label(text=section_name, bold=True, font_size=a*40, color=(1, 1, 1, 0.8))
             self.layout.add_widget(section_label)
             
             section_layout = GridLayout(cols=2)
             self.layout.add_widget(section_layout)
             
             for key, value in section_data.items():
-                label = Label(text=key, bold=True, font_size=30, color=(0, 0.5, 1, 0.8))
-                text_input = TextInput(text=value, multiline=False, font_size=20,height=35,size_hint_y=None)
+                label = Label(text=key, bold=True, font_size=a*30, color=(0, 0.5, 1, 0.8))
+                text_input = TextInput(text=value, multiline=False, font_size=a*20,height=a*35,size_hint_y=None)
                 
                 self.text_inputs[key] = text_input
                 
                 section_layout.add_widget(label)
                 section_layout.add_widget(text_input)
 
-        spacer = Widget(size_hint_y=None, height=70)
+        spacer = Widget(size_hint_y=None, height=a*70)
 
         self.layout.add_widget(spacer)
         button_layout = BoxLayout(orientation='horizontal', size_hint_y=None, height=80)
-        button_save = Button(text="SAVE", bold=True, font_size=40, size_hint=(None, None), size=(150, 60), color=(1, 1, 1, 0.8))
+        button_save = Button(text="SAVE", bold=True, font_size=a*40, size_hint=(None, None), size=(a*150, a*60), color=(1, 1, 1, 0.8))
         button_save.background_color = (0, 0.5, 1, 1)
         button_save.bind(on_press=self.save_json)
-        button_save.pos_hint = {'center_x': 0.3}
+        button_save.pos_hint = {'center_x': 0}
         button_layout.add_widget(button_save)
 
         self.run_button_original_color = (0, 0.5, 1, 1)
         self.stop_button_original_color = (0, 0.5, 1, 1)
 
-        button_run = Button(text="RUN", bold=True, font_size=40, size_hint=(None, None), size=(150, 60), color=(1, 1, 1, 0.8))
+        button_run = Button(text="RUN", bold=True, font_size=a*40, size_hint=(None, None), size=(a*150, a*60), color=(1, 1, 1, 0.8))
         button_run.background_color = (0, 0.5, 1, 1)
-        button_run.bind(on_press=self.run_code)  # Collegato a una nuova funzione "run_code"
+        button_run.bind(on_press=self.run_code)
         button_run.bind(on_release=self.reset_color)
-        button_run.pos_hint = {'center_x': 0.6}
+        button_run.pos_hint = {'center_x': 0.5}
         button_layout.add_widget(button_run)
 
-        button_stop = Button(text="STOP", bold=True, font_size=40, size_hint=(None, None), size=(150, 60), color=(1, 1, 1, 0.8))
+        button_stop = Button(text="STOP", bold=True, font_size=a*40, size_hint=(None, None), size=(a*150, a*60), color=(1, 1, 1, 0.8))
         button_stop.background_color = (0, 0.5, 1, 1)
-        button_stop.bind(on_press=self.stop_code)  # Collegato a una nuova funzione "run_code"
+        button_stop.bind(on_press=self.stop_code)
         button_stop.bind(on_release=self.reset_color)
-        button_stop.pos_hint = {'center_x': 0.9}
+        button_stop.pos_hint = {'center_x': 1}
         button_layout.add_widget(button_stop)
 
         self.layout.add_widget(button_layout)
@@ -79,7 +81,7 @@ class Arduino(App):
             data = json.load(f)
         return data
     def run_code(self, instance):
-        instance.background_color = (0, 1, 0, 1)
+        instance.background_color = (0, 1, 0, 0.5)
         self.arduino_process = subprocess.Popen(["python3", "../arduino.py"])
         self.result_label.text = "Running..."
     def stop_code(self, instance):
@@ -101,7 +103,6 @@ class Arduino(App):
         except Exception as e:
             self.result_label.text = f"Error: {e}"
     def reset_color(self, instance):
-        # Reimposta il colore originale del pulsante
         if instance.text == "RUN":
             instance.background_color = self.run_button_original_color
         elif instance.text == "STOP":
